ea8bad78aeaa712f8b5507d7d84cb998dd03b95b
Peach016 <serpostpostolov@yandex.ru>
2026-02-15 11:46:26 +0900

    Update separation.py

--- separation.py
+++ separation.py
@@ -1,8 +1,8 @@
-def separation(text: str, width: int):
+def separation(text: str, width: int, separator: str):
     a, lst = 0, list(text)
     for i in range(1, len(text)):
         if i % width == 0:
-            if a == 0: lst.insert(i, " ")
-            if a > 0: lst.insert(i + a, " ")
+            if a == 0: lst.insert(i, separator)
+            if a > 0: lst.insert(i + a, separator)
             a += 1
     return ''.join(lst)
